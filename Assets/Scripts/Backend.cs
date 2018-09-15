8298e07e7f739a88cbb40805a428f33c92c7a11b
Matti Kauppila <makaupp@gmail.com>
2018-09-15 03:37:34 +0300

    mongo driver cleaned up

--- Assets/Scripts/Backend.cs
+++ Assets/Scripts/Backend.cs
@@ -1,21 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using MongoDB.Driver.Builders;
+﻿using System;
 using MongoDB.Bson;
 using MongoDB.Driver;
-using MongoDB.Bson.Serialization.Attributes;
+using UnityEngine;
+
+public class User {
+	public int UserID { get; set; }
+	public float HighScore { get; set; }
+}
+
+public class Score {
+	public int UserID { get; set; }
+	public float score { get; set; }
+}
 
 public class Backend : MonoBehaviour {
 
 	public bool ConnectedToDB = false;
 	public bool BackendActive = true;
 	
-	private MongoClient client;
-	private MongoServer server;
 	private MongoDatabase db;
-	private MongoCollection<Order> orders;
-	
+	private MongoCollection<User> users;
+	private MongoCollection<Score> scores;
+
 	void Start () {
 		
 	}
