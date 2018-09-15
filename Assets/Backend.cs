cf37e3817d94227f196299730bbb12de35d9d73e
Matti Kauppila <makaupp@gmail.com>
2018-09-15 03:07:37 +0300

    MongoDB C# 1.11 driver added, Backend.cs skeleton

---
+++ Assets/Backend.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MongoDB.Driver.Builders;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Backend : MonoBehaviour {
+
+	public bool ConnectedToDB = false;
+
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
